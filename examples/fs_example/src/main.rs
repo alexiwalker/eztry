243d701666821e3db45f56f3325c15b6e160c42c
alex <alex.w@kzen8.com>
2025-03-03 14:34:49 +1000

    some minor extra stuff for cleanliness in an example

--- examples/fs_example/src/main.rs
+++ examples/fs_example/src/main.rs
@@ -1,6 +1,6 @@
 use retry_rs::prelude::*;
 
-use std::io::Write;
+use std::io::{Read, Write};
 use std::time::Duration;
 use std::sync::Arc;
 use std::fs;
@@ -34,12 +34,7 @@ async fn write_to_file(ctr:Control) -> RetryResult<(),()> {
 
     info!("fg_thread: Attempting to write to file");
 
-    let mut f = fs::OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .append(true)
-        .open("./test_file.txt").unwrap();
+    let mut f = get_file(false);
 
     match f.try_lock_exclusive() {
         Ok(_) => {
@@ -61,6 +56,24 @@ async fn write_to_file(ctr:Control) -> RetryResult<(),()> {
     }
 }
 
+
+fn get_file(lock: bool) -> fs::File {
+
+    let mut f = fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("./test_file.txt").unwrap();
+    
+    if lock {
+        f.try_lock_exclusive().expect("Failed to lock file");
+    }
+    
+    f
+
+}
+
 /*
 
 This example should always eventually succeed and exit - after the bg thread finishes 100 loops and releases the file
@@ -73,6 +86,8 @@ but backs off exponentially while it waits
 
 #[tokio::main]
 async fn main() {
+    
+    fs::exists("./test_file.txt").map(|_| fs::remove_file("./test_file.txt")).ok();
 
     env_logger::builder().filter_level(log::LevelFilter::Info).init();
 
@@ -93,26 +108,44 @@ async fn main() {
 
     let _ = thread.await;
 
+
+    /*
+    Ensure that the results of this are as expected:
+        - generates lines 1-99 (not 100 because it increments before the == 100 check, so the last line is 99)
+        - the last line is "Hello, world!" (can only write the last line after the background thread has released its lock after writing 1-99)
+    */
+    let mut f = get_file(false);
+    let mut out_str = String::new();
+    let _ = f.read_to_string(&mut out_str);
+    let lines = out_str.lines();
+    let count = lines.count();
+    info!("fg_thread: read {} lines", count);
+    
+    let last_line = out_str.lines().last().unwrap();
+    
+    
+    assert_eq!(count, 100);
+    assert_eq!(last_line, "Hello, world!");
+    
+    
+    fs::exists("./test_file.txt").map(|_| fs::remove_file("./test_file.txt")).ok();
+
+
 }
 
 
 /// This simulates a busy FS that may have file operations fail due to file contention
 async fn bg_thread(ctrl: Control) -> () {
 
-    { /* ensure the retry thread isnt running while we get the file*/
+    { 
+        /* ensure the retry thread isnt running while we get the file*/
+        /* the value of this mutex should already be run_bg when passed in, but for clarity*/
         let mut mg =ctrl.lock().await;
         *mg = ThreadControl::RunBackground;
     }
 
 
-    let mut f = fs::OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .append(true)
-        .open("./test_file.txt")
-        .unwrap();
-
+    let mut f = get_file(true);
 
     f.try_lock_exclusive().expect("bg_thread: Failed to lock file");
 
